c2c5ca0255703580ffaf3dce92c497d02adfabe4
rob <rob@linux.local>
2024-12-12 16:27:39 -0600

    Question 1

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,16 @@
-#add import
+import question_a
+
+def main():
+    while True:
+        user_input = int(input("[1] Display stock purchase history\n[2] Exit\n"))
+        if user_input == 1:
+            print()
+            question_a.stock_purchase_history()
+        elif user_input == 2:
+            print()
+            break
+        else:
+            print("\nYou must enter [1 or 2]")
+
+if __name__ == '__main__':
+    main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,31 @@
-#write functions here, don't add input('') statements here!
-def test_config():
-    return True
+
+class Stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+
+    def get_company_name(self):
+        return self.__company_name
+
+def stock_purchase_history():
+    AAPL = Stock("AAPL", "Apple Inc")
+    CAT = Stock( "CAT", "Caterpillar")
+    EK = Stock("EK", "Eastman Kodak")
+    GOOG = Stock("GOOG", "Google")
+    MSFT = Stock("MSFT", "Microsoft")
+
+    dictionary = {
+        "AAPL":AAPL,
+        "CAT":CAT,
+        "EK":EK,
+        "GOOG":GOOG,
+        "MSFT":MSFT
+    }
+
+    print("Symbol \t\t| Company Name")
+    print("_______________________________")
+    for entry in dictionary:
+        print(dictionary.get(entry).get_symbol(), "\t\t|", dictionary.get(entry).get_company_name())
