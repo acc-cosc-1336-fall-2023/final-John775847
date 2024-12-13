5d3b359d37f714f8d7829f0d745c349f92a6a51e
rob <rob@linux.local>
2024-12-13 17:24:13 -0600

    Question4

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,17 @@
-#add import
+import question_d
+
+def main():
+    stock_list = question_d.get_stock_list()
+    while True:
+        user_input = int(input("[1] Display stock purchase history\n[2] Exit\n"))
+        if user_input == 1:
+            print()
+            question_d.list_the_stocks(stock_list)
+        elif user_input == 2:
+            print()
+            break
+        else:
+            print("\nYou must enter [1 or 2]")
+
+if __name__ == '__main__':
+    main()
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,22 @@
-#write functions here, don't add input('') statements here!
+class Stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+
+    def get_company_name(self):
+        return self.__company_name
+
+def get_stock_list():
+    AAPL = Stock("AAPL", "Apple Inc")
+    CAT = Stock( "CAT", "Caterpillar")
+    EK = Stock("EK", "Eastman Kodak")
+    GOOG = Stock("GOOG", "Google")
+    MSFT = Stock("MSFT", "Microsoft")
+    return [AAPL, CAT, EK, GOOG, MSFT]
+
+def list_the_stocks(stock_list):
+    for entry in stock_list:
+        print(entry.get_symbol(), "\t\t|", entry.get_company_name())
